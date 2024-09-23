b942245914be9ef39d28182d742bccaa06c39418
Holden Seidel <business@rdstudios.org>
2024-09-23 09:59:55 -0700

    i guess idk how to use ptrs, kinda fixed

--- src/lexer/tokenizer.hpp
+++ src/lexer/tokenizer.hpp
@@ -9,19 +9,19 @@ namespace FoxLang {
 class Tokenizer {
 public:
   Tokenizer(std::string *start)
-      : input(start), start(&start[0]), current(&start[0]) {}
+      : input(start), start(&(*start)[0]), current(&(*start)[0]) {}
 
   std::optional<std::vector<Token>> TokenizeInput() {
-    std::cout << "Start: " << *start << std::endl;
-    std::cout << "Input: " << *input << std::endl;
-    std::cout << "Input[0]: " << &input[0] << std::endl;
+    std::cout << "Start: " << start << std::endl;
+    std::cout << "Input: " << input << std::endl;
+    std::cout << "Input[0]: " << (*input)[0] << std::endl;
     return {};
   }
 
 private:
   std::string *input;
-  std::string *start;
-  std::string *current;
+  char *start;
+  char *current;
   int line = 1;
 };
 
