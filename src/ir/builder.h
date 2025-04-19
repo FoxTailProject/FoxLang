a35436c35694d5274b25ede963ec121d3eeb511f
Holden Seidel <business@rdstudios.org>
2025-04-19 16:07:57 -0700

    More builder stuff + value type

--- src/ir/builder.h
+++ src/ir/builder.h
@@ -4,20 +4,31 @@
 #include <string>
 #include <vector>
 
+#include "container.h"
 #include "namespace.h"
+#include "value.h"
 
 namespace FoxLang::IR {
 class Builder {
 public:
 	Builder()
-		: nsStack(), currentNamespace(nullptr), insertPoint(nullptr), nses() {}
+		: nsStack(), currentNamespace(nullptr), ipStack(), insertPoint(nullptr),
+		  nses() {}
 
-	void createFunction(std::string name);
+	Container *createFunction(std::string name);
+	void createAdd(Value *left, Value *right);
+	void createSub(Value *left, Value *right);
+	void createMul(Value *left, Value *right);
+	void createDiv(Value *left, Value *right);
+	Container *createStruct(std::string name);
+	void createFunctionCall(Container *function);
+	Value *createVariable(std::string name);
 
 private:
 	std::stack<Namespace *> nsStack;
+	std::stack<Container *> ipStack;
 	Namespace *currentNamespace;
-	void *insertPoint;
+	Container *insertPoint;
 	std::vector<Namespace> nses;
 };
 } // namespace FoxLang::IR
---
+++ src/ir/value.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace FoxLang::IR {
+class Value {
+public:
+	Value();
+};
+} // namespace FoxLang::IR
